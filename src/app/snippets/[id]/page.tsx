796078b74fd807508a0b5c43112d1256d125fbfe
chanakaHetti <cdileep40@gmail.com>
2024-11-09 14:55:56 +0000

    Adding the loading spinner for server components

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -10,6 +10,9 @@ interface SnippetViewPageProps {
 
 export default async function SnippetViewPage(props: SnippetViewPageProps) {
   const { id } = await props.params;
+
+  await new Promise((r) => setTimeout(r, 2000));
+
   const snippet = await db.snipet.findFirst({
     where: { id: parseInt(id) },
   });
