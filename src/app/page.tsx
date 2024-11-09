4fbf37206349c12f7a9f09528638cb8c14b75411
chanakaHetti <cdileep40@gmail.com>
2024-11-09 18:04:45 +0000

    Adding the tailwind classes for styiling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
 import { db } from '@/db';
+import Link from 'next/link';
 
 export default async function Home() {
   const snippets = await db.snipet.findMany();
 
   const renderedSnippets = snippets.map((snippet) => (
-    <div key={snippet.id}>{snippet.title}</div>
+    <Link
+      key={snippet.id}
+      href={`snippets/${snippet.id}`}
+      className="flex justify-between items-center p-2 border rounded"
+    >
+      <div>{snippet.title}</div>
+      <div>View</div>
+    </Link>
   ));
 
-  return <div className="">{renderedSnippets}</div>;
+  return (
+    <div className="">
+      <div className="flex m-2 mb-10 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link href={`snippets/new`} className="border p-2 rounded">
+          New Snippet
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
+    </div>
+  );
 }
