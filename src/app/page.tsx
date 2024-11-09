a5377a1803aff3badeb56cf49b6602a5d3713ee7
chanakaHetti <cdileep40@gmail.com>
2024-11-09 14:22:16 +0000

    Adding the fetching snippet data and show list on the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div className="">Home Page</div>;
+import { db } from '@/db';
+
+export default async function Home() {
+  const snippets = await db.snipet.findMany();
+
+  const renderedSnippets = snippets.map((snippet) => (
+    <div key={snippet.id}>{snippet.title}</div>
+  ));
+
+  return <div className="">{renderedSnippets}</div>;
 }
